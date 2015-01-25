c50cf388a80f79d32b9aa3f5aaf35f802d055854
Stefan Ric <stfric369@gmail.com>
2015-01-25 22:07:38 +0100

    Update

--- install.sh
+++ install.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
-(( EUID != 0 )) && exec sudo -- "$0" "$@"
-
 read -p "Do you want to replace the default MEGAsync status icons with the new ones [Y/n]?is
 This will install a patched version sni-qt thus replacing the default one!" yn
 
 if [[ $yn =~ ^[Yy]$ ]] || [[ $yn == "" ]]; then
     
     echo "Adding the repository"
-    add-apt-repository --yes ppa:rpeshkov/ppa
+    sudo add-apt-repository --yes ppa:cybre/sni-qt-eplus
+
     
     echo "Updating package list"
-    apt-get -y update
+    sudo apt-get -y update
     
-    echo "Upgrading system"
-    apt-get -y upgrade
-    
+    echo "Upgrading packages"
+    sudo apt-get -y install sni-qt sni-qt:i386
+
     echo "Creating directory..."
-    mkdir -p /usr/share/sni-qt/icons
+    mkdir -p $HOME/.local/share/sni-qt/icons
     
     echo "Copying files to the directory..."
-    cp ./icons/* /usr/share/sni-qt/icons/
+    cp ./icons/* $HOME/.local/share/sni-qt/icons/
     
     echo "Reverting MEGAsync application launchers..."
     newpath="Exec=$HOME/.bin/megasync-wrapper/megasync.sh"
-    sed -i "s|"$newpath"|Exec=megasync|g" /usr/share/applications/megasync.desktop
+    sudo sed -i "s|"$newpath"|Exec=megasync|g" /usr/share/applications/megasync.desktop
     sed -i "s|"$newpath"|Exec=megasync|g" $HOME/.config/autostart/megasync.desktop
-    
-    echo "Removing the repository"
-    add-apt-repository --remove --yes ppa:rpeshkov/ppa
     
     echo
     echo "You can now start MEGAsync from the applications menu and enjoy the new icons!"
